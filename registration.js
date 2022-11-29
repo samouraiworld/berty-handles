bfc3687ee3736669a85e1bc846312b5e02e25ec7
TS <t.scandolera@gmail.com>
2022-11-29 04:24:10 +0100

    TS wip: Registration 1.1 & 1.2 responsiveness done, ISSUE : Unable to set scroll ON for Registration steps

--- registration.js
+++ registration.js
@@ -1,3 +1,20 @@
+// Navbar
+const bar = document.getElementById("bar");
+const close = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+    bar.addEventListener("click", () => {
+        nav.classList.add("active");
+    })
+}
+
+if (close) {
+    close.addEventListener("click", () => {
+        nav.classList.remove("active");
+    })
+}
+
 // trigger
 const btn = document.getElementById("continue-btn");
 
