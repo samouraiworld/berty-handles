d37ec00d2d074699a1e04e841904a32abe3f6598
TS <t.scandolera@gmail.com>
2022-11-30 16:46:17 +0100

    TS: mobile pop-up implemented | wip : card-popup, step array (again)

--- registration2.js
+++ registration2.js
@@ -14,3 +14,25 @@ if (close) {
         nav.classList.remove("active");
     })
 }
+
+// trigger
+const trig = document.getElementById("profile-popup");
+const closetrig = document.getElementById("clickable");
+const mobilepopup = document.getElementById("mobile-popup");
+const img = document.getElementById("popup-img");
+
+if (trig) {
+    trig.addEventListener("click", () => {
+        mobilepopup.classList.add("fullscreen");
+        closetrig.classList.add("enabled");
+        img.classList.add("enabled");
+    })
+}
+
+if (closetrig) {
+    closetrig.addEventListener("click", () => {
+        img.classList.remove("enabled");
+        closetrig.classList.remove("enabled");
+        mobilepopup.classList.remove("fullscreen");
+    })
+}
