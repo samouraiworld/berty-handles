d03ff416f7e203e08d32fe44df9fccb11b8831ad
Mouhamed Wade <echo1006j@gmail.com>
2023-01-20 16:12:45 +0100

    add linter and prettier

--- berty-handles/components/footer/footer.tsx
+++ berty-handles/components/footer/footer.tsx
@@ -1,15 +1,18 @@
-import styles from '../../styles/Home.module.css'
-
+import styles from '../../styles/Home.module.css';
 
 export default function Footer() {
-    return(
-        <section className={styles.footer}>
-        <div className={styles.colContainer}>
+  return (
+    <section className={styles.footer}>
+      <div className={styles.colContainer}>
         <div className={styles.col1}>
-          <img className={styles.logo} src="../img/berty_footer_logo.svg" alt="Berty' society logo"/>
+          <img
+            className={styles.logo}
+            src="../img/berty_footer_logo.svg"
+            alt="Berty' society logo"
+          />
           <p>The Berty app is developed by the NGO Berty Technologies.</p>
         </div>
-  
+
         <div className={`${styles.col2} ${styles.strong}`}>
           <a href="#">Features</a>
           <a href="#">Challenges</a>
@@ -17,7 +20,7 @@ export default function Footer() {
           <a href="#">Community</a>
           <a href="#">Blog</a>
         </div>
-  
+
         <div className={styles.col2}>
           <h4>Developpers</h4>
           <a href="#">Docs</a>
@@ -25,7 +28,7 @@ export default function Footer() {
           <a href="#">Contribute</a>
           <a href="#">Projects we love</a>
         </div>
-  
+
         <div className={styles.col2}>
           <h4>About Us</h4>
           <a href="#">Jobs</a>
@@ -35,21 +38,23 @@ export default function Footer() {
           <a href="#">Store</a>
           <a href="#">Assets</a>
         </div>
-        </div>
-  
-        <div className={styles.copyright}>
+      </div>
+
+      <div className={styles.copyright}>
         <div className={styles.copyrightTxt}>
-          <p>Berty™️ is a registered trademark © 2018-2022 Berty Technologies - All right reserved - Terms & Conditions -
-            Privacy Policy</p>
+          <p>
+            Berty™️ is a registered trademark © 2018-2022 Berty Technologies - All right reserved -
+            Terms & Conditions - Privacy Policy
+          </p>
         </div>
         <div className={styles.follow}>
-          <img className={styles.logo} src="../img/discord_logo.svg" alt="Discord logo"/>
-          <img className={styles.logo} src="../img/twitter_logo.svg" alt="Tweeter logo"/>
-          <img className={styles.logo} src="../img/github_logo.svg" alt="Github logo"/>
-          <img className={styles.logo} src="../img/unknown_logo.svg" alt="Unknown logo"/>
-          <img className={styles.logo} src="../img/linkedin_logo.svg" alt="Linkedin logo"/>
+          <img className={styles.logo} src="../img/discord_logo.svg" alt="Discord logo" />
+          <img className={styles.logo} src="../img/twitter_logo.svg" alt="Tweeter logo" />
+          <img className={styles.logo} src="../img/github_logo.svg" alt="Github logo" />
+          <img className={styles.logo} src="../img/unknown_logo.svg" alt="Unknown logo" />
+          <img className={styles.logo} src="../img/linkedin_logo.svg" alt="Linkedin logo" />
         </div>
       </div>
-      </section>
-    )
+    </section>
+  );
 }
